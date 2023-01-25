b951ba2a3ba73629c558d4bea055001d15bf41a9
ulimsss <z_o__o@naver.com>
2023-01-25 18:56:26 +0900

    배포 수정

--- src/constants/login.ts
+++ src/constants/login.ts
@@ -1,4 +1,4 @@
-export const CLIENT_KEY = 'e894e54ca9adb616e5d741dff2d4f59f';
-export const CLIENT_SECRET = 'BsRfueOGy0FIWOeloIW98hs9VklKeFPr';
+export const CLIENT_KEY = 'e894e54ca9adb61161e5d1741dff2d4f59f1';
+export const CLIENT_SECRET = 'BsRfueOGy10FIWO1eloIW981hs9Vk1lKeFPr';
 export const REDIRECT_URI = 'http://localhost:5174/oauth/kakao/callback';
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
